cd19325e76f8c5c808cf7c52622dc5dfd202180a
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-03 14:43:10 +0530

    updated

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 
-const app=express()
-app.listen(8080,()=>{
-    mongoose.connect("mongodb://localhost:27017/gcet")
-    console.log("Server Started");
-});
-
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.use("/users", userRouter);
 
 app.use("/products", productRouter);
-app.use(cors());
-app.use(express.json());
+
+app.listen(8080, () => {
+  mongoose.connect("mongodb://localhost:27017/gcet");
+  console.log("Server Started");
+});
 // app.get("/",(req,res)=>{
 //     return res.send("Good Morning");
 // });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,4 +1,4 @@
-mport express from 'express'
+import express from 'express'
 import productModel from "../models/userModel.js";
 
 const productRouter = express.Router()
