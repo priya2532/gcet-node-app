72f3118598cf67052e1d0fd04357d2cf10712319
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-03 14:38:46 +0530

    updated

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose';
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+
 const app=express()
 app.listen(8080,()=>{
     mongoose.connect("mongodb://localhost:27017/gcet")
     console.log("Server Started");
 });
-const userSchema=mongoose.Schema({
-    name:{type:String},
-    email:{type:String},
-    pass:{type:String},
-});
-const productSchema=mongoose.Schema({
-    name:{type:String},
-    price:{type:Number}
-});
-// const user=mongoose.model("User",userSchema);
-const product=mongoose.model("product",productSchema);
+
+
+app.use("/users", userRouter);
+
+app.use("/products", productRouter);
 app.use(cors());
 app.use(express.json());
-app.get("/",(req,res)=>{
-    return res.send("Good Morning");
-});
-app.post("/register",async(req,res)=>{
-    const {name,email,pass}=req.body
-    const result=await user.insertOne({name:name,email:email,pass:pass});
-    return res.json(result);
-});
-app.post("/login",async(req,res)=>{
-    const {name,email,pass}=req.body
-    const result=await user.insertOne({email:email,pass:pass});
-    if(result) return res.json({message:"Accepted"})
-        else{
+// app.get("/",(req,res)=>{
+//     return res.send("Good Morning");
+// });
+// app.post("/register",async(req,res)=>{
+//     const {name,email,pass}=req.body
+//     const result=await productModel.insertOne({name:name,email:email,pass:pass});
+//     return res.json(result);
+// });
+// app.post("/login",async(req,res)=>{
+//     const {name,email,pass}=req.body
+//     const result=await user.insertOne({email:email,pass:pass});
+//     if(result) return res.json({message:"Accepted"})
+//         else{
     
-    return res.json("Invalid");
-        }
-});
-app.get("/products",async(req,res)=>{
-    const products=await product.find();
-    res.json(products);
-})
+//     return res.json("Invalid");
+//         }
+// });
+// app.get("/products",async(req,res)=>{
+//     const products=await product.find();
+//     res.json(products);
+// })
 
-app.get("/greet",(req,res)=>{res.send("Greetings")})
-app.get("/name",(req,res)=>{res.send("Sri Priya")})
-app.get("/weather",(req,res)=>{res.send("45 degrees")})
+//app.get("/greet",(req,res)=>{res.send("Greetings")})
+//app.get("/name",(req,res)=>{res.send("Sri Priya")})
+//app.get("/weather",(req,res)=>{res.send("45 degrees")})
 // const product=()=>{
 //     return[
 //         { id: 1, name: "Laptop", price: 50000 },
