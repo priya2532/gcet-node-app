b66fa9c6836917e14567b26aab7082909a48513c
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-03 11:23:58 +0530

    updated

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ import cors from 'cors'
 import mongoose from 'mongoose';
 const app=express()
 app.listen(8080,()=>{
-    mongoose.connect("mongodb:")
+    mongoose.connect("mongodb://localhost:27017/gcet")
     console.log("Server Started");
 });
+const userSchema=mongoose.Schema({
+    name:{type:String},
+});
+const user=mongoose.model("User",userSchema);
 app.use(cors());
 app.get("/",(req,res)=>{
     return res.send("Good Morning");
+});
+app.get("/register",async(req,res)=>{
+    const result=await user.insertOne({name:"John"});
+    return res.json(result);
 });
 app.get("/greet",(req,res)=>{res.send("Greetings")})
 app.get("/name",(req,res)=>{res.send("Sri Priya")})
