add0021749ea6679109534ce6c9395d738e99a47
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-05 11:33:12 +0530

    updated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,4 +8,10 @@ productRouter.get("/all", async (req, res) => {
   res.json(products);
 });
 
+productRouter.post("/new", async (req, res) => {
+  const product = req.body
+  const products = await productModel.create(product);
+  res.json(products);
+});
+
 export default productRouter
