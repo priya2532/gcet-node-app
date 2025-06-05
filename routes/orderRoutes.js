6af9be774e992bf30a8cab3f7a26a66bbbb19fbf
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-05 11:39:37 +0530

    updated

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,8 +4,20 @@ import orderModel from "../models/orderModel.js";
 const orderRouter = express.Router();
 
 orderRouter.post("/new", async (req, res) => {
-  const { email, price } = req.body;
-  const result = orderModel.insertOne({ email, price });
+  const { email, orderValue } = req.body;
+  const result = await orderModel.insertOne({ email, orderValue });
+  return res.json(result);
+});
+
+orderRouter.get("/:id", async (req, res) => {
+  const email = req.params.id;
+  const result = await orderModel.find({ email });
+  return res.json(result);
+});
+
+
+orderRouter.get("/all", async (req, res) => {
+  const result = await orderModel.find();
   return res.json(result);
 });
 
