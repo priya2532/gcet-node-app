9d173f7dc027df383faaa4f49ca174e7ef202d81
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-05 09:49:48 +0530

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 const DBUSER=encodeURIComponent(process.env.DBUSER)
 const DBPASS=encodeURIComponent(process.env.DBPASS)
-const MONGODB_uri=`mongodb+srv://${DBUSER}:${DBPASS}@cluster0.o8uwfuw.mongodb.net/gcet?retryWrites=true&w=majority&appName=Cluster0`
+const MONGODB_URI=`mongodb+srv://${DBUSER}:${DBPASS}@cluster0.o8uwfuw.mongodb.net/gcet?retryWrites=true&w=majority&appName=Cluster0`
 //const MONGODB_URI = process.env.MONGODB_URI
 
 app.use("/users", userRouter);
