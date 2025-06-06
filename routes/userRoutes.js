bd73aad7709678ce88ffc4111c9da7bf7127b31a
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-06 14:59:51 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,13 @@ const userRouter = express.Router()
 
 userRouter.post("/register", async (req, res) => {
   const { name, email, pass } = req.body;
-  const result = await userModel.insertOne({ name: name, email: email, pass: pass });
+  const result = await userModel.insertOne({ name: name, email: email, pass: hashpassword });
   return res.json(result);
 });
 
 userRouter.post("/login", async (req, res) => {
   const { email, pass } = req.body;
+  const hashpassword=await bcrypt.hash.(pass,10);
   const result = await userModel.findOne({ email, pass });
   if (!result) return res.json({ message: "Invalid user or password" });
   return res.json(result);
